273f413cacb9b39e6710b88d1e740512df54f7b3
akitobora <akitobora@gmail.com>
2025-06-27 18:14:15 +0300

    fix errors

---
+++ tests/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="uks_checker",
+    version="0.1.0",
+    packages=find_packages(),       # найдёт uks_checker
+    install_requires=[
+        "requests",
+        "beautifulsoup4",
+        "python-telegram-bot",
+        # и всё, что ещё в requirements.txt
+    ],
+    python_requires=">=3.8",
+)
