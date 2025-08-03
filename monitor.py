59616d1ef54dc3694fa95ee5360bafbc6bf696cf
akitobora <91880105+akitobora@users.noreply.github.com>
2025-08-04 02:34:20 +0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -264,11 +264,13 @@ def main():
     app.add_handler(CommandHandler("getnews",cmd_getnews))
 
     logger.info("Bot started, polling…")
+    """
     app.run_webhook(
     listen="0.0.0.0",
     port=8080,
     webhook_url="https://bba7ujaae80r5nogivh1.containers.yandexcloud.net/"
     )
-
+"""
+    application.run_polling()
 if __name__ == "__main__":
     main()
